44e072b75552a2d6a226ddbca74b966ede748d64
Grigoriy Baranov <baranov@w3m.co>
2020-02-09 16:08:07 +0300

    add Authority

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@ use std::io::Error;
 pub struct Query {
     pub data: String,
 }
-
 impl Query {
     pub fn new(data: String) -> Query {
         Query { data }
@@ -19,5 +18,24 @@ impl Query {
             }
         }
         Ok(Query { data: string })
+    }
+}
+
+pub struct Authority {
+    host: String,
+    port: Option<String>,
+}
+impl Authority {
+    pub fn new(host: String, port: Option<String>) -> Authority {
+        Authority { host, port }
+    }
+    pub fn authority(&self) -> String {
+        let mut string = String::new();
+        string.push_str(self.host.as_str());
+        match &self.port {
+            Some(port) => string.push_str(format!(":{}", port).as_str()),
+            None => {}
+        }
+        string
     }
 }
