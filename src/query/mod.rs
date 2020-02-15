61faa1d6ef29f3506094a33771c98dadb9bfed39
Grigoriy Baranov <baranov@w3m.co>
2020-02-15 20:59:13 +0300

    impl Default for Query

--- src/query/mod.rs
+++ src/query/mod.rs
@@ -6,22 +6,10 @@ pub struct Query {
     key_value_components: Option<Vec<KeyValueComponent>>,
 }
 
-impl Query {
-    pub fn new() -> Query {
+impl Default for Query {
+    fn default() -> Query {
         Query {
             key_value_components: None,
-        }
-    }
-    pub fn add_kvc(&mut self, data: KeyValueComponent) {
-        match &mut self.key_value_components {
-            Some(kvcs) => {
-                kvcs.push(data);
-            }
-            None => {
-                let mut vec = Vec::<KeyValueComponent>::new();
-                vec.push(data);
-                self.key_value_components = Some(vec);
-            }
         }
     }
 }
@@ -42,5 +30,23 @@ impl ToString for Query {
             None => {}
         }
         s
+    }
+}
+
+impl Query {
+    pub fn new() -> Query {
+        Self::default()
+    }
+    pub fn add_kvc(&mut self, data: KeyValueComponent) {
+        match &mut self.key_value_components {
+            Some(kvcs) => {
+                kvcs.push(data);
+            }
+            None => {
+                let mut vec = Vec::<KeyValueComponent>::new();
+                vec.push(data);
+                self.key_value_components = Some(vec);
+            }
+        }
     }
 }
