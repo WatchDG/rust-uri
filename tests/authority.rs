c9b71cf5703ae93632015418a2d5c7318822cdd7
Grigoriy Baranov <baranov@w3m.co>
2020-02-16 14:12:53 +0300

    add test for Authority string_repr()

--- tests/authority.rs
+++ tests/authority.rs
@@ -1,11 +1,13 @@
+use wdg_uri::authority::{Authority, Host, Port};
+
 #[cfg(test)]
 mod authority {
-    use string_repr::StringRepr;
-    use wdg_uri::authority::*;
+    use super::{Authority, Host, Port};
     mod host {
-        use super::{Host, StringRepr};
+        use super::Host;
         mod string_repr {
-            use super::{Host, StringRepr};
+            use super::Host;
+            use string_repr::StringRepr;
             #[test]
             fn t0() {
                 assert_eq!(Host::new("127.0.0.1".into()).string_repr(), "127.0.0.1");
@@ -18,6 +20,29 @@ mod authority {
             fn t2() {
                 assert_eq!(Host::new("example.com".into()).string_repr(), "example.com");
             }
+        }
+    }
+    mod port {
+        use super::Port;
+        mod string_repr {
+            use super::Port;
+            use string_repr::StringRepr;
+            #[test]
+            fn t0() {
+                assert_eq!(Port::new("80".into()).string_repr(), "80");
+            }
+        }
+    }
+    mod string_repr {
+        use super::{Authority, Host, Port};
+        use string_repr::StringRepr;
+        #[test]
+        fn t0() {
+            let host = Host::new("localhost".into());
+            let port = Port::new("8080".into());
+            let mut authority = Authority::new(host);
+            authority.set_port(port);
+            assert_eq!(authority.string_repr(), "localhost:8080");
         }
     }
 }
