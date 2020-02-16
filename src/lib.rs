78d3e22f2ec4981000edf82aa643f01a5849e4c5
Grigoriy Baranov <baranov@w3m.co>
2020-02-16 23:35:36 +0300

    impl StringRepr for URI

--- src/lib.rs
+++ src/lib.rs
@@ -6,18 +6,37 @@ pub mod query;
 pub mod scheme;
 
 use authority::Authority;
+use path::Path;
 use scheme::Scheme;
+use string_repr::StringRepr;
 
 pub struct URI {
     scheme: Scheme,
     pub authority: Option<Authority>,
+    path: Path,
 }
 
 impl URI {
-    fn new(scheme: Scheme) -> URI {
+    pub fn new(scheme: Scheme, path: Path) -> URI {
         URI {
             scheme,
             authority: None,
+            path,
         }
+    }
+    pub fn set_authority(&mut self, authority: Authority) {
+        self.authority = Some(authority);
+    }
+}
+
+impl StringRepr for URI {
+    fn string_repr(&self) -> String {
+        let mut string = format!("{}:", self.scheme.string_repr());
+        match &self.authority {
+            Some(authority) => string.push_str(format!("//{}", authority.string_repr()).as_ref()),
+            None => {}
+        }
+        string.push_str(self.path.string_repr().as_ref());
+        string
     }
 }
