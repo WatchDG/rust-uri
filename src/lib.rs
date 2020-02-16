a51fc25cf1fea069e48c674ece95ddb53fdb5aea
Grigoriy Baranov <baranov@w3m.co>
2020-02-16 19:07:07 +0300

    add struct URI

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,15 @@ extern crate string_repr;
 pub mod authority;
 pub mod path;
 pub mod query;
+
+use authority::Authority;
+
+pub struct URI {
+    authority: Authority,
+}
+
+impl URI {
+    fn new(authority: Authority) -> URI {
+        URI { authority }
+    }
+}
