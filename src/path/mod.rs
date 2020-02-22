2dbf56fa8c83c98a87816a67779276fc0df9a3a3
Grigoriy Baranov <baranov@w3m.co>
2020-02-22 10:56:02 +0300

    update

--- src/path/mod.rs
+++ src/path/mod.rs
@@ -1,8 +1,17 @@
 use string_repr::StringRepr;
 
+/// The path component contains data, usually organized in hierarchical form, that, along with data
+/// in the non-hierarchical query component, serves to identify a resource within the scope of the
+/// URI's scheme and naming authority (if any).
 pub struct Path(String);
 
 impl Path {
+    /// Create new Path.
+    /// # Example:
+    /// ```
+    /// use wdg_uri::path::Path;
+    /// let path = Path::new("/".into());
+    /// ```
     pub fn new(data: String) -> Path {
         Path(data)
     }
