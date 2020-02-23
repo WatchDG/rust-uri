b149ae0d9d55438ed1369a4f8ab06e494fc826dc
Grigoriy Baranov <baranov@w3m.co>
2020-02-23 16:50:16 +0300

    add validate() to Host

--- src/authority/host.rs
+++ src/authority/host.rs
@@ -1,5 +1,13 @@
+use regex::Regex;
 use std::fmt;
 use string_repr::StringRepr;
+
+lazy_static! {
+    static ref HOST_IPv4address_RE: Regex = Regex::new(
+        r"^(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)$"
+    )
+    .unwrap();
+}
 
 pub struct Host(String);
 
@@ -12,6 +20,34 @@ impl Host {
     /// ```
     pub fn new(data: String) -> Host {
         Host(data)
+    }
+
+    /// Validate Host.
+    /// # Support:
+    /// * IPv4Address
+    /// # Example:
+    /// ```
+    /// use wdg_uri::authority::Host;
+    /// let host = Host::new("127.0.0.1".into());
+    /// if !host.validate() {
+    ///     panic!("fail");
+    /// }
+    /// ```
+    pub fn validate(&self) -> bool {
+        HOST_IPv4address_RE.is_match(&self.0)
+    }
+
+    /// Check if Host is IPv4Address.
+    /// # Example:
+    /// ```
+    /// use wdg_uri::authority::Host;
+    /// let host = Host::new("127.0.0.1".into());
+    /// if host.is_ipv4addr() {
+    ///    println!("Host is IPv4Address.");
+    /// }
+    /// ```
+    pub fn is_ipv4addr(&self) -> bool {
+        HOST_IPv4address_RE.is_match(&self.0)
     }
 }
 
