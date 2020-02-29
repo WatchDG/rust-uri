c729173d0f1dee0d3d29afe425b4b98f51ed9a9a
Grigoriy Baranov <baranov@w3m.co>
2020-02-29 08:55:30 +0300

    update

--- src/authority/mod.rs
+++ src/authority/mod.rs
@@ -4,6 +4,7 @@ mod user_info;
 
 pub use host::*;
 pub use port::*;
+use regex::{Error, Regex};
 use string_repr::StringRepr;
 pub use user_info::*;
 
@@ -21,8 +22,23 @@ impl<'a> Authority<'a> {
             user_info: None,
         }
     }
+    pub fn get_host(&self) -> &Host {
+        &self.host
+    }
+    pub fn get_port(&self) -> Option<&Port> {
+        match &self.port {
+            Some(port) => Some(port),
+            None => None,
+        }
+    }
     pub fn set_port(&mut self, port: Port<'a>) {
         self.port = Some(port);
+    }
+    pub fn get_user_info(&self) -> Option<&UserInfo> {
+        match &self.user_info {
+            Some(user_info) => Some(user_info),
+            None => None,
+        }
     }
     pub fn set_user_info(&mut self, user_info: UserInfo<'a>) {
         self.user_info = Some(user_info);
@@ -42,6 +58,36 @@ impl<'a> StringRepr for Authority<'a> {
             None => {}
         }
         string
+    }
+}
+
+pub trait AuthorityParse {
+    fn parse(&self) -> Result<Authority<'_>, Error>;
+}
+
+impl AuthorityParse for &str {
+    fn parse(&self) -> Result<Authority, Error> {
+        lazy_static! {
+            static ref AUTHORITY_PARSE_RE: Regex = Regex::new(
+                r"(?x)
+            ^(?:([^@])+@)?(?P<host>[^:]+)(?::(?P<port>.*))?$
+            "
+            )
+            .unwrap();
+        }
+        let captures = AUTHORITY_PARSE_RE.captures(&self).unwrap();
+        Ok(Authority {
+            host: captures
+                .name("host")
+                .map(|host| Host::new(host.as_str()))
+                .unwrap(),
+            port: captures
+                .name("port")
+                .map_or(None, |port| Some(Port::new(port.as_str()))),
+            user_info: captures
+                .name("user_info")
+                .map_or(None, |user_info| Some(UserInfo::new(user_info.as_str()))),
+        })
     }
 }
 
